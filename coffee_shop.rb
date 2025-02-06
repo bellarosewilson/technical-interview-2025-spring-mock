139d232a282f54e72f3a10e21b6b9af1c07e3546
Bella Rose Wilson <bwilson9893@gmail.com>
2025-02-06 19:25:23 +0000

    initial commit

--- coffee_shop.rb
+++ coffee_shop.rb
@@ -7,7 +7,7 @@ class CoffeeShop
     @inventory = Inventory.new
   end
 
-  def self.lace_order(type, size)
+  def place_order(type, size)
     coffee = Coffee.new(type, size) 
     puts "📖 Placed order for a #{coffee.description}"  
     @orders.push(coffee)
